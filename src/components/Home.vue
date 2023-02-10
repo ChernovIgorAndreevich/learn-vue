2069893466607a23252ba0bfbd447d91dfb3b844
aleksandrpikulin <apikulin437@gmail.com>
2023-02-10 05:41:03 +0400

    issues/tasks#1 form-compleate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,60 @@
 <template>
-  <div>
-    <h1>Привет заебал</h1>
+  <div class="container">
+    <div class="container-item">
+      <div v-bind:class="{'form-error': $v.login.$invalid}">
+        Login: <input v-model="login">
+      </div>
+      <div>
+        Password: <input v-model="password">
+      </div>
+    <button v-bind:disabled="$v.login.$invalid"
+      @click="onSubmit">Submit</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { required, numeric } from 'vuelidate/lib/validators'
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      console.log(123, this.login, this.password)
+    }
+  },
+  validations () {
+    return {
+      login: {
+        required,
+        numeric
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.form-error {
+    color: red;
+  }
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .container-item {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      input {
+        margin: 10px;
+      }
+    }
+  }
 </style>
